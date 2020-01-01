870d5531f16976e7e5bc657dd625274c1a8b7da0
Taylor Cooke <taylorcooke@gmail.com>
2019-12-31 20:28:31 -0500

    city weather showing via API

--- the_weather/weather/views.py
+++ the_weather/weather/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
+import requests
 
 def index(request):
-    return render(request, 'weather/index.html')
+
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=fd5060bdf66d198bf9a19861e3e4a92c'
+    city = 'Las Vegas'
+    city_weather = requests.get(url.format(city)).json()
+
+    weather = {
+        'city': city,
+        'temperature': city_weather['main']['temp'],
+        'description': city_weather['weather'][0]['description'],
+        'icon': city_weather['weather'][0]['icon']
+    }
+
+    context = {'weather': weather}
+
+    return render(request, 'weather/index.html', context)
