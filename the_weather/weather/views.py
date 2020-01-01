b7bc47f0e0ec6e1af3f5a79dd489996dd5c9808a
Taylor Cooke <taylorcooke@gmail.com>
2019-12-31 20:43:30 -0500

    displaying multiple cities

--- the_weather/weather/views.py
+++ the_weather/weather/views.py
@@ -1,19 +1,25 @@
 from django.shortcuts import render
 import requests
+from .models import City
 
 def index(request):
 
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=fd5060bdf66d198bf9a19861e3e4a92c'
-    city = 'Las Vegas'
-    city_weather = requests.get(url.format(city)).json()
+    cities = City.objects.all()
+    weather_data=[]
 
-    weather = {
-        'city': city,
-        'temperature': city_weather['main']['temp'],
-        'description': city_weather['weather'][0]['description'],
-        'icon': city_weather['weather'][0]['icon']
-    }
+    for city in cities:
+        city_weather = requests.get(url.format(city)).json()
 
-    context = {'weather': weather}
+        weather = {
+            'city': city,
+            'temperature': city_weather['main']['temp'],
+            'description': city_weather['weather'][0]['description'],
+            'icon': city_weather['weather'][0]['icon']
+        }
+
+        weather_data.append(weather)
+
+    context = {'weather_data': weather_data}
 
     return render(request, 'weather/index.html', context)
